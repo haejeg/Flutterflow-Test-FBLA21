055c3a01aa1da4d83ec21adcf848515613e530dd
haejeg <88898962+haejeg@users.noreply.github.com>
2021-12-21 00:39:48 -0700

    Changed Mainscreen secondary font, added settings screen, made custom sliders (custom_thumb & custom_track), added sleep function for static classes but there may be a better method to do it. I think thread sleep might not work as intended (we'll see tomorrow after experimentation.) also organize codenz tmr

--- app/src/main/java/com/example/app/Settings.java
+++ app/src/main/java/com/example/app/Settings.java
@@ -5,21 +5,15 @@ import static com.example.app.StaticClasses.sleep;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class Settings extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_settings);
         getSupportActionBar().hide();
-
-        //this is temporary lol
-        Intent intent = new Intent(MainActivity.this, Settings.class);
-        startActivity(intent);
-        return;
     }
 }
--- app/src/main/java/com/example/app/StaticClasses.java
+++ app/src/main/java/com/example/app/StaticClasses.java
@@ -4,4 +4,16 @@ public class StaticClasses {
     public static int randomIntFrom(int minimum, int maximum) {
         return 1; //temp
     }
+
+    public static void sleep(int ms) {
+        //Sleep function
+        try
+        {
+            Thread.sleep(ms);
+        }
+        catch(InterruptedException ex)
+        {
+            Thread.currentThread().interrupt();
+        }
+    }
 }
