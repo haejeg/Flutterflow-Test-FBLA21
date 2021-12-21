8ff7a45e852e99b938b947c296f11451816c2965
haejeg <88898962+haejeg@users.noreply.github.com>
2021-12-20 18:17:16 -0700

    - Added loading screen - Added settings icon - Added Background image for dark mode - Added raleway font - Changed activity settings - Fixed errors - Updated Misc - Added placeholder for static classes

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+
     }
 }
---
+++ app/src/main/java/com/example/app/StaticClasses.java
@@ -0,0 +1,7 @@
+package com.example.app;
+
+public class StaticClasses {
+    public static int randomIntFrom(int minimum, int maximum) {
+        return 1; //temp
+    }
+}
